20f82ce0df27059ce598a41ec46bce348934a382
chaluemwut <chaluemwut.dev@myorder.ai>
2024-08-25 13:32:17 +0700

    add api integration testing

--- api/src/user_coin/user_coin.controller.spec.ts
+++ api/src/user_coin/user_coin.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserCoin } from './user_coin.entity';
 import { DataSource, Repository } from 'typeorm';
+import { CanActivate } from '@nestjs/common';
+import { UserCoinController } from './user_coin.controller';
+import { AuthGuard } from '../auth/auth.guard';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserCoinModule } from './user_coin.module';
 
 describe('UserCoinController', () => {
     let userRepository: Repository<UserCoin>;
@@ -33,3 +39,43 @@ describe('UserCoinController', () => {
         });
     });
 });
+
+// let controller: UserCoinController;
+// beforeAll(async () => {
+//     const mockGuard: CanActivate = { canActivate: jest.fn(() => true) };
+
+//     const module: TestingModule = await Test.createTestingModule({
+//         imports: [
+//             UserCoinModule,
+//             ConfigModule.forRoot({ isGlobal: true }),
+//             TypeOrmModule.forRoot({
+//                 type: 'mysql',
+//                 host: 'localhost',
+//                 port: 3306,
+//                 username: 'root',
+//                 password: 'Rvpooh123',
+//                 database: 'cryptodb',
+//                 entities: ['./**/*.entity.ts'],
+//                 synchronize: false,
+//             }),
+//         ],
+//         controllers: [UserCoinController],
+//         // providers: [{ provide: AuthGuard, useValue: mockGuard }],
+//     })
+//         .overrideGuard(AuthGuard)
+//         .useValue(mockGuard)
+//         .compile();
+
+//     controller = module.get<UserCoinController>(UserCoinController);
+// });
+
+// describe('test controller', () => {
+
+
+//     describe('controller test', () => {
+//         // it('test hi', (done) => {
+//         //     expect(controller.query()).toBe('hi');
+//         //     done();
+//         // });
+//     });
+// });
--- api/src/user_profile/user_profile.controller.spec.ts
+++ api/src/user_profile/user_profile.controller.spec.ts
@@ -1,8 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserProfile } from './user_profile.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
 import { DataSource, Repository } from 'typeorm';
+import { UserProfile } from './user_profile.entity';
+import { UserProfileModule } from './user_profile.module';
 
-describe('UserProfileController', () => {
+describe('API Unit testing UserProfileController', () => {
     let userRepository: Repository<UserProfile>;
     const dataSource = {
         createEntityManager: jest.fn()
@@ -36,3 +41,54 @@ describe('UserProfileController', () => {
         });
     });
 });
+
+describe('API Integration GET /users-profile/save and /login', () => {
+
+    let app: INestApplication;
+    let repository: Repository<UserProfile>;
+
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            imports: [
+                UserProfileModule,
+                ConfigModule.forRoot({ isGlobal: true }),
+                TypeOrmModule.forRoot({
+                    type: 'mysql',
+                    host: 'localhost',
+                    port: 3306,
+                    username: 'root',
+                    password: 'Rvpooh123',
+                    database: 'cryptodb',
+                    entities: ['./**/*.entity.ts'],
+                    synchronize: false,
+                }),
+            ],
+        }).compile();
+        app = module.createNestApplication();
+        await app.init();
+        repository = module.get("UserProfileRepository");
+    });
+
+    afterAll(async () => {
+        await repository.query(`DELETE FROM user_profile where email = 'abc@abc.com';`);
+        await app.close();
+    });
+
+    describe('login test', () => {
+        it('should can registration and login', async () => {
+            const response = await request(app.getHttpServer())
+                .post('/user-profile/save')
+                .send({ 'email': 'abc@abc.com', 'password': '1234' })
+
+            const { body } = await request(app.getHttpServer())
+                .post('/user-profile/login')
+                .send({ 'email': 'abc@abc.com', 'password': '1234' })
+                .set('Accept', 'application/json');
+            expect({ auth: body['auth'], message: body['message'] }).toEqual(
+                { auth: 1, message: 'pass' }
+            )
+        });
+    })
+
+});
+
